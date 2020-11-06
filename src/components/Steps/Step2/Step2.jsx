9cc98e0baaa348868f3ff9eaa4b57dc180124e47
Izquiano <izquiano@hotmail.com>
2020-11-06 23:41:23 +0100

    Google Maps Api Key

--- src/components/Steps/Step2/Step2.jsx
+++ src/components/Steps/Step2/Step2.jsx
@@ -43,7 +43,7 @@ navigator.geolocation.getCurrentPosition(function (position) {
 
 const Step2 = ({ nextStep, handleChangeSelected, selected }) => {
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: "AIzaSyAEQa3PYaWe_yOmF1oCTNJyxBr1-7SI9fY", 
     libraries,
   });
   const [markers, setMarkers] = React.useState([]);

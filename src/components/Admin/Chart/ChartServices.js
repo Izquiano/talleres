eeff97a6c7352e9b16e3a256907b6bf2a13f93b5
Izquiano <izquiano@hotmail.com>
2020-11-07 02:32:24 +0100

    segmentos barras

--- src/components/Admin/Chart/ChartServices.js
+++ src/components/Admin/Chart/ChartServices.js
@@ -99,14 +99,14 @@ const ChartServices = () => {
             scales: {
               yAxes: [
                 {
-                  display: true,
+                  display: false,
                   ticks: {
                     autoSkip: true,
                     maxTicksLimit: 10,
                     beginAtZero: true,
                   },
                   gridLines: {
-                    display: true,
+                    display: false,
                   },
                 },
               ],
